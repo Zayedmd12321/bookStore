b059638a0045c60294d7c1cd5f8ea2490a2cc7be
Md Zayed Ghanchi <eagle.zayed@gmail.com>
2025-03-22 00:19:34 +0530

    bookSection

--- home.js
+++ home.js
@@ -1,3 +1,276 @@
+const fiction = [
+  {
+    "title": "The Cold Dish",
+    "author": "Craig Johnson",
+    "about": "Set in the remote town of Durant, Wyoming, this mystery novel follows Sheriff Walt Longmire...",
+    "price": "450",
+    "image": "images/books/book10.jpeg",
+    "category": "Fiction"
+  },
+  {
+    "title": "We Do Not Part",
+    "author": "Hang Kung",
+    "about": "This poignant and heart-wrenching novel explores the complexities of love and separation...",
+    "price": "499",
+    "image": "images/books/book1.jpeg",
+    "category": "Fiction"
+  },
+  {
+    "title": "River Sing Me Home",
+    "author": "Amanda",
+    "about": "Set against the backdrop of colonialism and its lasting impacts, this novel tells the story of a mother's desperate search...",
+    "price": "550",
+    "image": "images/books/book2.jpeg",
+    "category": "Fiction"
+  },
+  {
+    "title": "The Bluest Eye",
+    "author": "Toni Morrison",
+    "about": "This powerful and haunting novel explores the themes of race, beauty, and identity in America...",
+    "price": "499",
+    "image": "images/books/book8.jpeg",
+    "category": "Fiction"
+  },
+  {
+    "title": "Pride and Prejudice",
+    "author": "Jane Austen",
+    "about": "A beloved classic of English literature, 'Pride and Prejudice' is a timeless exploration of love, social class, and personal growth...",
+    "price": "299",
+    "image": "images/books/book3.jpeg",
+    "category": "Fiction"
+  },
+  {
+    "title": "The Divine Comedy",
+    "author": "Dante Alighieri",
+    "about": "One of the most important works in Western literature, 'The Divine Comedy' is an epic poem...",
+    "price": "799",
+    "image": "images/books/book17.jpg",
+    "category": "Fiction"
+  },
+  {
+    "title": "The Starless Sea",
+    "author": "Erin Morgenstern",
+    "about": "A mysterious underground world filled with books, secret doors, and timeless stories. Zachary Ezra Rawlins discovers a strange book that leads him on a journey through a labyrinth of hidden stories, secret societies, and fate.",
+    "price": "499",
+    "image": "images/books/book19.jpeg",
+    "category": "Fiction", 
+  },
+  {
+    "title": "Circe",
+    "author": "Madelline Miller",
+    "about": "A mysterious underground world filled with books, secret doors, and timeless stories. Zachary Ezra Rawlins discovers a strange book that leads him on a journey through a labyrinth of hidden stories, secret societies, and fate.",
+    "price": "499",
+    "image": "images/books/book20.jpg",
+    "category": "Fiction", 
+  },
+  {
+    "title": "Jane Eyre",
+    "author": "Charlotte Brontë",
+    "about": "A mysterious underground world filled with books, secret doors, and timeless stories. Zachary Ezra Rawlins discovers a strange book that leads him on a journey through a labyrinth of hidden stories, secret societies, and fate.",
+    "price": "499",
+    "image": "images/books/book21.jpeg",
+    "category": "Fiction", 
+  },
+];
+
+const fantasy = [
+  {
+    "title": "Fantastic Beasts",
+    "author": "J.K. Rowling",
+    "about": "Set in the magical world of wizards, this story follows Newt Scamander...",
+    "price": "599",
+    "image": "images/books/book18.jpg",
+    "category": "Fantasy"
+  },
+  {
+    "title": "His Majesty's Dragon",
+    "author": "Naomi Novik",
+    "about": "In this historical fantasy novel, the Napoleonic Wars take a magical turn...",
+    "price": "599",
+    "image": "images/books/book11.jpeg",
+    "category": "Fantasy"
+  },
+  {
+    "title": "The Maiden",
+    "author": "J.R. Towns",
+    "about": "In this epic fantasy novel, a young woman embarks on a dangerous journey...",
+    "price": "399",
+    "image": "images/books/book14.jpg",
+    "category": "Fantasy"
+  },
+  {
+    "title": "Get in Trouble",
+    "author": "Kelly Link",
+    "about": "A collection of short stories that blur the lines between fantasy, horror, and magical realism...",
+    "price": "550",
+    "image": "images/books/book16.jpg",
+    "category": "Fantasy"
+  }
+];
+
+const horror = [
+  {
+    "title": "Psycho",
+    "author": "Robert Bloch",
+    "about": "A classic psychological thriller that delves deep into the disturbed mind of Norman Bates...",
+    "price": "350",
+    "image": "images/books/book12.jpg",
+    "category": "Horror"
+  },
+  {
+    "title": "Jaws",
+    "author": "Peter Benchley",
+    "about": "Set on the fictional island of Amity, 'Jaws' tells the chilling story of a giant great white shark...",
+    "price": "499",
+    "image": "images/books/book15.jpg",
+    "category": "Horror"
+  },
+    {
+      "title": "Mexican Gothic",
+      "author": "Silvia Moreno-Garcia",
+      "about": "A chilling gothic horror novel set in a mysterious, decaying mansion in Mexico.",
+      "price": 599,
+      "image": "images/books/book30.jpeg",
+      "category": "Horror"
+    },
+    {
+      "title": "The Only Good Indians",
+      "author": "Stephen Graham Jones",
+      "about": "A haunting tale of supernatural revenge and cultural identity.",
+      "price": 649,
+      "image": "images/books/book31.jpeg",
+      "category": "Horror"
+    },
+    {
+      "title": "House of Leaves",
+      "author": "Mark Z. Danielewski",
+      "about": "A mind-bending, terrifying exploration of a house that defies reality.",
+      "price": 799,
+      "image": "images/books/book32.jpeg",
+      "category": "Horror"
+    },
+    {
+      "title": "The Silent Companions",
+      "author": "Laura Purcell",
+      "about": "A gothic ghost story filled with eerie dolls and unsettling mysteries.",
+      "price": 549,
+      "image": "images/books/book33.jpeg",
+      "category": "Horror"
+    },
+];
+
+const science = [
+  {
+    "title": "City of Orange",
+    "author": "David Yoon",
+    "about": "In a world shattered by catastrophe, a man wakes up in a desert city with no memory of his past...",
+    "price": "699",
+    "image": "images/books/book4.jpeg",
+    "category": "Science"
+  },
+  {
+    "title": "The Brilliant Abyss",
+    "author": "Helen Scales",
+    "about": "A captivating look at the wonders of the deep sea and its creatures.",
+    "price": 649,
+    "image": "images/books/book29.jpeg",
+    "category": "Science"
+  },
+  {
+    "title": "The Otherwhere Post",
+    "author": "John C. Hall",
+    "about": "This science fiction novel takes readers on an exhilarating journey through alternate realities...",
+    "price": "399",
+    "image": "images/books/book5.jpeg",
+    "category": "Science"
+  },
+  {
+    "title": "The Order of Time",
+    "author": "Carlo Rovelli",
+    "about": "A mind-bending exploration of time and its mysteries in physics.",
+    "price": 599,
+    "image": "images/books/book26.jpg",
+    "category": "Science"
+  },
+  {
+    "title": "Otherlands: A Journey Through Earth's Extinct Worlds",
+    "author": "Thomas Halliday",
+    "about": "A mesmerizing journey into prehistoric landscapes and lost worlds.",
+    "price": 699,
+    "image": "images/books/book27.jpeg",
+    "category": "Science"
+  },
+  {
+    "title": "The Hidden Reality",
+    "author": "Brian Greene",
+    "about": "An intriguing dive into parallel universes and the multiverse theory.",
+    "price": 799,
+    "image": "images/books/book28.jpeg",
+    "category": "Science"
+  },
+];
+
+const nonfiction = [
+  {
+    "title": "Autocracy, Inc.",
+    "author": "Anna D. Bostwick",
+    "about": "In this in-depth exploration, the author examines the inner workings of autocratic regimes...",
+    "price": "799",
+    "image": "images/books/book6.jpeg",
+    "category": "Non-Fiction"
+  },
+  {
+    "title": "The New Map",
+    "author": "Daniel Yergin",
+    "about": "This comprehensive exploration of the global energy landscape examines the shifting dynamics of power...",
+    "price": "850",
+    "image": "images/books/book7.jpeg",
+    "category": "Non-Fiction"
+  },
+  {
+    "title": "The Book of Delights",
+    "author": "Ross Gay",
+    "about": "A collection of lyrical essays celebrating the small joys in life.",
+    "price": 499,
+    "image": "images/books/book22.jpg",
+    "category": "Nonfiction"
+  },
+  {
+    "title": "The Soul of an Octopus",
+    "author": "Sy Montgomery",
+    "about": "A fascinating journey into the intelligence and mystery of octopuses.",
+    "price": 599,
+    "image": "images/books/book23.jpg",
+    "category": "Nonfiction"
+  },
+  {
+    "title": "Entangled Life",
+    "author": "Merlin Sheldrake",
+    "about": "A deep dive into the world of fungi and their influence on our planet.",
+    "price": 699,
+    "image": "images/books/book24.jpg",
+    "category": "Nonfiction"
+  },
+  {
+    "title": "Atlas of the Heart",
+    "author": "Brené Brown",
+    "about": "An exploration of human emotions and meaningful connections.",
+    "price": 799,
+    "image": "images/books/book25.jpeg",
+    "category": "Nonfiction"
+  },
+];
+
+
+const catagories = [fiction, nonfiction, science, horror, fantasy];
+const fictionContainer = document.getElementById('fiction');
+const nonfictionContainer = document.getElementById('nonfiction');
+const scienceContainer = document.getElementById('science');
+const horrorContainer = document.getElementById('horror');
+const fantasyContainer = document.getElementById('fantasy');
+const categoryContainer = [fictionContainer, nonfictionContainer, scienceContainer, horrorContainer, fantasyContainer];
+
+
 //Adding hamberger menu
   function hamberger() {
     document.getElementsByClassName('dropdown-menu')[0].style.display = "none";
@@ -62,4 +335,63 @@ function typeText() {
     }
 }
 
-typeText();  
+typeText();
+
+window.addEventListener("scroll", function () {
+  let navbar = document.querySelector("#navbar");
+
+  if (window.scrollY > 50) 
+      navbar.style.background = "#23282f"; 
+  else
+      navbar.style.background = "transparent";
+});
+
+//Adding books
+function AddBook(catagory,Container){
+  catagory.forEach(book => {
+    Container.innerHTML += `
+      <div class="bookCard">
+        <img src="${book.image}" alt="">
+      
+      <div class="overlay">
+        <a href="" class="bookmark"><svg xmlns="http://www.w3.org/2000/svg" height="18px"
+                          viewBox="0 -960 960 960" width="24px" fill="#fff">
+                          <path
+                              d="m480-120-58-52q-101-91-167-157T150-447.5Q111-500 95.5-544T80-634q0-94 63-157t157-63q52 0 99 22t81 62q34-40 81-62t99-22q94 0 157 63t63 157q0 46-15.5 90T810-447.5Q771-395 705-329T538-172l-58 52Zm0-108q96-86 158-147.5t98-107q36-45.5 50-81t14-70.5q0-60-40-100t-100-40q-47 0-87 26.5T518-680h-76q-15-41-55-67.5T300-774q-60 0-100 40t-40 100q0 35 14 70.5t50 81q36 45.5 98 107T480-228Zm0-273Z" />
+                      </svg></a>
+          <h3>${book.title}</h3>
+          <p>-${book.author}</p>
+          <div class="buttons">
+              <button>Details</button>
+              <button>Buy</button>
+          </div>
+      </div>
+      </div>
+    `;
+  });
+  catagory.forEach(book => {
+    Container.innerHTML += `
+      <div class="bookCard">
+        <img src="${book.image}" alt="">
+      
+      <div class="overlay">
+        <a href="" class="bookmark"><svg xmlns="http://www.w3.org/2000/svg" height="18px"
+                          viewBox="0 -960 960 960" width="24px" fill="#fff">
+                          <path
+                              d="m480-120-58-52q-101-91-167-157T150-447.5Q111-500 95.5-544T80-634q0-94 63-157t157-63q52 0 99 22t81 62q34-40 81-62t99-22q94 0 157 63t63 157q0 46-15.5 90T810-447.5Q771-395 705-329T538-172l-58 52Zm0-108q96-86 158-147.5t98-107q36-45.5 50-81t14-70.5q0-60-40-100t-100-40q-47 0-87 26.5T518-680h-76q-15-41-55-67.5T300-774q-60 0-100 40t-40 100q0 35 14 70.5t50 81q36 45.5 98 107T480-228Zm0-273Z" />
+                      </svg></a>
+          <h3>${book.title}</h3>
+          <p>-${book.author}</p>
+          <div class="buttons">
+              <button>Details</button>
+              <button>Buy</button>
+          </div>
+      </div>
+      </div>
+    `;
+  });
+};
+
+for (let i = 0; i < catagories.length; i++) {
+  AddBook(catagories[i],categoryContainer[i]);
+}
